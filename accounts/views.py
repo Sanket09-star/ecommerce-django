1802755d33fd178ede667b03bca8f481322beb53
sanket ghode <sanketrghode@gmail.com>
2025-10-07 15:07:22 +0530

    Enhance cart functionality for authenticated users, including product variation handling and quantity updates

--- accounts/views.py
+++ accounts/views.py
@@ -15,6 +15,7 @@ from django.contrib.auth.tokens import default_token_generator
 
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
+import requests
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -66,14 +67,53 @@ def login(request):
                 is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
                 if is_cart_item_exists:
                     cart_item = CartItem.objects.filter(cart=cart)
+                    
+                    #Getting the product variations by cart id
+                    product_variation = []
                     for item in cart_item:
-                        item.user = user #assign the user to the cart item
-                        item.save()
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    #product_variation = [1,2,3,4,5,6]
+                    #ex_var_list = [4,6,3,5]
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity +=1 # increase the cart item quantity
+                            item.user = user #assign the user to the cart item
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart = cart)
+                            for item in cart_item:
+                                item.user = user #assign the user to the cart item
+                                item.save()
+                                        
             except:
                 pass
             auth.login(request, user)
             messages.success(request, 'You are now logged in.')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER') # get the previous url
+            try:
+                query = requests.utils.urlparse(url).query #get the query parameter
+                #next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))#split the query parameter
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)  
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
