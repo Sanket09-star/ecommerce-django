9f1b7af8b11294f3db53ec3f69e0a80095477534
sanket ghode <sanketrghode@gmail.com>
2025-10-21 11:10:40 +0530

    solve allowed host error

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['greatkart-1rr3.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
