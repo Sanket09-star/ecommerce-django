27a942f74abd10da00518941d5a77c3f90635390
sanket ghode <sanketrghode@gmail.com>
2025-10-21 13:37:44 +0530

    Configured Supabase IPv4 database connection 1

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['greatkart-1rr3.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
